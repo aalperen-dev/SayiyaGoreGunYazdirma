e38a4a849f705d6c24561ce4cfbdf2590d28da0c
celcius911 <aalperen@anadolu.edu.tr>
2022-04-30 14:36:21 +0300

    Add project files.

---
+++ SayiyaGoreGunYazdirma/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SayiyaGoreGunYazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Klavyeden 1 ve 7 arasında bir sayı isteyen, girilen sayı sırasındaki
+            //haftanın gününü veren C# konsol uygulamasının kodları
+            //switch-case yapısını kullanarak aşağıdaki boşluğa yazın.
+
+            Console.Write("1 ile 7 arasında bir sayı girin : ");
+            int sayi = int.Parse(Console.ReadLine());
+            string gun = "";
+            //string turunden tanımlanmıs değişkeni null'dan çıkarttım.
+            switch (sayi)
+            {
+                case 1:
+                    gun = "Pazartesi";
+                    break;
+                case 2:
+                    gun = "Salı";
+                    break;
+                case 3:
+                    gun = "Çarşamba";
+                    break;
+                case 4:
+                    gun = "Perşembe";
+                    break;
+                case 5:
+                    gun = "Cuma";
+                    break;
+                case 6:
+                    gun = "Cumartesi";
+                    break;
+                case 7:
+                    gun = "Pazar";
+                    break;
+                default:
+                    Console.WriteLine("Yanlış değer girdiniz!");
+                    break;
+            }
+            if (sayi > 0 && sayi < 8)
+            {
+                Console.WriteLine("Haftanın " + sayi + ". günü : " + gun);
+            }
+
+        }
+    }
+}
